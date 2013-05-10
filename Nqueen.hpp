dbffb9fe8a6b8af1fab8ecba2d880c808fe5d527
fang zhiwen <zhiwenf@gmail.com>
2013-05-10 10:46:45 +0800

    add NqueenCounter by bit manipulation

---
+++ Nqueen.hpp
@@ -0,0 +1,28 @@
+
+/*
+ * count the toale number of N queue by bitwise
+ */
+
+class NqueenCounter {
+public:
+    int totalNQueens(int n) {
+        return nQueen(0, 0, 0, n);
+    }
+    
+	/*
+	 * use tree parameter of indicate the forbiden postion by row and the two diagonals
+	 */
+    int nQueen(int row, int r, int d, int n) {
+        if (row == (1<<n) - 1) {
+            return 1;
+        }
+        int sum = 0;
+        int v = row | r | d;
+     	while (((~v) & (v+1)) < (1<<n)) {
+			sum += nQueen(row | ((~v) & (v+1)), (r<<1) | (((~v) & (v+1))<<1), (d>>1) | (((~v) & (v+1))>>1), n);
+			v |= ((~v) & (v+1));
+        }
+        return sum;
+    }
+};
+
